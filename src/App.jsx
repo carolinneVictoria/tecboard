cb232ea71bee1f2943f30f66fa7ae5475774925e
Carol <carolinevictoria1224@gmail.com>
2026-05-13 11:50:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css'
 
+function TituloFormulario (props) {
+  return (
+    <h2>{props.children}</h2>
+  )
+}
+
+function CampoFormulario ({children}) {
+  return (
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+function Label({children, htmlFor}){
+  return (
+    <label htmlFor={htmlFor}>{children}</label>
+  )
+}
+
 function FormularioDeEvento () {
 
   return (
     <form className="form-evento">
-      <h2>Preencha para criar um evento:</h2>
-      <fieldset>
-        <label htmlFor="">Qual o nome do evento?</label>
-        <input type="text" id='nome'/>
-      </fieldset>
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+      <CampoFormulario>
+        <Label htmlFor="nome">Qual o nome do evento?</Label>
+        <input type="text" id='nome' placeholder='Summer dev hits'/>
+      </CampoFormulario>
     </form>
   )
 }
